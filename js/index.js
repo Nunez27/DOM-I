21f28bffed927837e9f20fbb279aff2f56006958
Brian Nunez <nunez27@Brians-MacBook-Pro.local>
2021-04-05 21:44:17 -0400

    Cleaned up my code

--- js/index.js
+++ js/index.js
@@ -62,63 +62,57 @@ const color = document.querySelectorAll('nav a')
 
 color.forEach(item => item.style.color = 'green')
 
-
+//Header
 const h1 = document.querySelector('h1');
 h1.textContent = siteContent.cta['h1'];
 
+//Button
 const button = document.querySelector('button');
 button.textContent = siteContent.cta['button'];
 
+//Img
 const firstImg = document.querySelector('#cta-img');
 firstImg.setAttribute('src', siteContent['cta']['img-src']);
 
 const textButton = document.querySelector('.cta-text button');
 textButton.textContent = siteContent['cta']['button'];
 
+//Main
 const features = document.querySelector('.top-content .text-content:nth-of-type(1) h4')
 features.innerText = siteContent['main-content']['features-h4']
-
 const featuresText = document.querySelector('.top-content .text-content:nth-of-type(1) p');
 featuresText.textContent = siteContent['main-content']['features-content'];
-
 const about = document.querySelector('.text-content:nth-of-type(2) h4')
 about.innerText = siteContent["main-content"]["about-h4"]
-
 const aboutText = document.querySelector('.text-content:nth-of-type(2) p')
 aboutText.innerText = siteContent["main-content"]["about-content"]
-
 const middleImg = document.querySelector('#middle-img')
 middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
+//Bottom
 const service = document.querySelector('.bottom-content .text-content h4')
 service.innerText = siteContent["main-content"]["services-h4"]
-
 const serviceText = document.querySelector('.bottom-content .text-content p')
 serviceText.innerText = siteContent["main-content"]["services-content"]
-
 const product = document.querySelector('.bottom-content .text-content:nth-of-type(2) h4')
 product.innerText = siteContent["main-content"]["product-h4"]
-
 const productText = document.querySelector('.bottom-content .text-content:nth-of-type(2) p')
 productText.innerText = siteContent["main-content"]["product-content"]
-
 const vision = document.querySelector('.bottom-content .text-content:nth-of-type(3) h4')
 vision.innerText = siteContent["main-content"]["vision-h4"]
-
 const visionText = document.querySelector('.bottom-content .text-content:nth-of-type(3) p')
 visionText.innerText = siteContent["main-content"]["vision-content"]
 
+//Contact
 const contact = document.querySelector('.contact h4')
 contact.innerText = siteContent['contact']["contact-h4"]
-
 const contactAddress = document.querySelector('.contact p')
 contactAddress.innerText = siteContent['contact']['address']
-
 const contactPhone = document.querySelector('.contact p:nth-of-type(2)')
 contactPhone.innerText = siteContent['contact']['phone']
-
 const contactEmail = document.querySelector('.contact p:nth-of-type(3)')
 contactEmail.innerText = siteContent['contact']['email']
 
+//Footer
 const footer = document.querySelector('footer p')
 footer.innerText = siteContent['footer']['copyright']
